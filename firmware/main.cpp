3a40f522ba24010552c265a116e7957a1590b3cd
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2018-01-12 17:18:12 +0100

    ejemplo: control bucle abierto PWM con dos botones

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -10,6 +10,8 @@ Universidad de Almeria
 #include "libclaraquino/leds.h"
 #include "libclaraquino/delays.h"
 #include "libclaraquino/millis_timer.h"
+#include "libclaraquino/pwm.h"
+#include "libclaraquino/gpio.h"
 
 #include <avr/interrupt.h> // sei()
 
@@ -18,6 +20,18 @@ int main(void)
 	// ================== Setup hardware ==================
 	UART::Configure(500000);
 	millis_init();
+
+	// OC0A=PIN PB3 (=0x23): pwm output
+	gpio_pin_mode(0x23,OUTPUT);
+	pwm_init(PWM_TIMER0,PWM_PRESCALER_1);
+
+	int16_t pwm_val = 10;
+
+	const uint8_t PIN_BTN_UP = 0x10;  // A0
+	const uint8_t PIN_BTN_DOWN = 0x11;  // A1
+
+	gpio_pin_mode(PIN_BTN_UP, INPUT_PULLUP);
+	gpio_pin_mode(PIN_BTN_DOWN, INPUT_PULLUP);
 
 	// Enable interrupts:
 	sei();
@@ -29,6 +43,20 @@ int main(void)
 	while(1)
 	{
 		// ---- Run scheduled tasks ----------
+		if (!gpio_pin_read(PIN_BTN_UP))
+		{
+			++pwm_val;
+			if (pwm_val>255) pwm_val= 255;
+		}
+		if (!gpio_pin_read(PIN_BTN_DOWN))
+		{
+			--pwm_val;
+			if (pwm_val<0) pwm_val= 0;
+		}
 
+		pwm_set_duty_cycle(PWM_TIMER0, PWM_PIN_OCnA, pwm_val);
+
+
+		delay_ms(100);
 	}
 }
